e48e0bca0eab21b9014e49988ff52f404ff8dcf2
jngermon <jn.germon@gmail.com>
2023-03-09 08:57:36 +0100

    Export Normalized interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import { Container } from './container'
-import { Environment } from './environment'
+import { Container, ContainerNormalized } from './container'
+import { Environment, EnvironmentNormalized } from './environment'
 import { getKernel, setKernel } from './instance'
-import { Kernel } from './kernel'
-import { Manifest } from './manifest'
+import { Kernel, KernelNormalized } from './kernel'
+import { Manifest, ManifestNormalized } from './manifest'
 
 export {
     Container,
+    ContainerNormalized,
     Environment,
+    EnvironmentNormalized,
     getKernel,
     Kernel,
+    KernelNormalized,
     Manifest,
+    ManifestNormalized,
     setKernel,
 }
